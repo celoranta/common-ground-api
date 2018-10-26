b3dcce4751c247897d92ca42c2cde2381bf6cbe6
Christoper <celoranta@gmail.com>
2018-10-26 10:15:28 -0700

    Set up basic server requirements

---
+++ index.js
@@ -0,0 +1,56 @@
+
+//Include frameworks
+const fs = require('fs');
+const url = require('url');
+const express = require('express');
+const path = require('path');
+const bodyparser = require('body-parser');
+require('dotenv').config(); // For reading .env variables
+//const nodemailer = require('nodemailer');
+
+//Instantiate managers
+var app = express();
+
+//Assign variables
+var httpPort = process.env.PORT || 8000;
+var hostname = "localhost";
+
+//Create filepaths
+//  var publicFolder = path.join(__dirname + '/public');
+
+var apiLanding = path.join(__dirname + '/views/landing.html');
+// var temperror = path.join(__dirname + '/views/temp-error.html');
+// var tempsuccess = path.join(__dirname + '/views/temp-success.html');
+
+//Static Routes
+// app.use(express.static(publicFolder));
+//Tell express to use body parser and not parse extended bodies directly
+app.use(bodyparser.urlencoded({ extended: true }))
+
+//Standard Routes
+// app.get('/', (req, res) => {
+//   res.sendFile(homePage);
+// });
+app.get('/', (req, res) => {
+  res.sendFile(apiLanding);
+});
+
+// POST route from contact form
+// 
+
+// 404
+app.use(function(req, res, next) {
+  return res.status(404).send({ message: 'Route '+req.url+' Not found.' });
+});
+
+// 500 - Any server error
+app.use(function(err, req, res, next) {
+  return res.status(500).send({ error: err });
+});
+
+app.listen(httpPort);
+
+
+
+
+
